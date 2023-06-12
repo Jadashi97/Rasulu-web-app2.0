4e0b47e00483965fb0615ffd99a4f0306ba7fa29
jadashi97 <njadauniversal23@yahoo.com>
2023-06-12 18:55:37 -0500

    (fix): added title and logo

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <Wrapper>
       <MainHeaderPage>
+        <TitleImage src="https://cdn.pixabay.com/photo/2019/10/11/09/16/bank-4541449_640.jpg"/>
+        <Title>
             rasulu
+        </Title>
       </MainHeaderPage>
     </Wrapper>
   )
@@ -14,9 +17,17 @@ const Header = () => {
 export default Header;
 
 const Wrapper = tw.div`
-    flex
+flex flex-col h-screen w-full md: w-auto bg-white p-4
 `;
 
 const MainHeaderPage = tw.div`
-    flex p-6 
+     flex flex-1 flex-col justify-between
+`;
+
+const TitleImage = tw.img`
+    h-32 object-contain
+`;
+
+const Title = tw.div`
+    text-4xl text-black p-4 mt-8 text-center bg-gradient-to-r from-purple-400 md:from-yellow-500
 `;
