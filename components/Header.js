04350424443d7086750d5e5baffe8e5ef99f26d2
jadashi97 <njadauniversal23@yahoo.com>
2023-06-13 15:58:40 -0500

    (fix): moved logo and header portion to index

--- components/Header.js
+++ components/Header.js
@@ -3,31 +3,25 @@ import tw from 'tailwind-styled-components';
 
 const Header = () => {
   return (
-    <Wrapper>
       <MainHeaderPage>
-        <TitleImage src="https://cdn.pixabay.com/photo/2019/10/11/09/16/bank-4541449_640.jpg"/>
+        <TitleImage src="https://cdn.pixabay.com/photo/2020/04/18/14/58/money-5059442_640.jpg"/>
         <Title>
             rasulu
         </Title>
       </MainHeaderPage>
-    </Wrapper>
   )
 }
 
 export default Header;
 
-const Wrapper = tw.div`
-flex flex-col h-screen w-full md: w-auto bg-white p-4
-`;
-
 const MainHeaderPage = tw.div`
-     flex flex-1 flex-col justify-between
+     flex flex-row h-scre
 `;
 
 const TitleImage = tw.img`
-    h-32 object-contain
+   flex-1 h-16 object-contain
 `;
 
 const Title = tw.div`
-    text-4xl text-black p-4 mt-8 text-center bg-gradient-to-r from-purple-400 md:from-yellow-500
+    text-4xl text-black p-4
 `;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
-import Header from '../components/Header';
 
 const index = () => {
   return (
     <Wrapper>
-      <Header/>
+      <ActionItems>
+         <HeaderBox>
+            <HeaderLogo src='https://cdn.pixabay.com/photo/2020/04/18/14/58/money-5059442_640.jpg'/>
+            <CompanyName>rasulu</CompanyName>
+         </HeaderBox>
+
+      </ActionItems>
     </Wrapper>
   )
 }
@@ -14,5 +19,22 @@ export default index;
 
 
 const Wrapper = tw.div`
-    flex flex-col h-screen
+ flex flex-col h-screen w-full md: w-auto bg-white p-4
+`;
+
+const ActionItems = tw.div`
+  flex flex-1 p-4
+`;
+
+const HeaderBox = tw.div`
+  flex flex-1 justify-between
+`;
+
+
+const HeaderLogo = tw.img`
+  h-28 w-20
+`;
+
+const CompanyName = tw.div`
+  h-28
 `;
