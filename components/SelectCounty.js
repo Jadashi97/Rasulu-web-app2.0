cecd223fbd4308d7553ec2954eb37d24e9a65b7a
jadashi97 <njadauniversal23@yahoo.com>
2023-06-14 21:05:24 -0500

    (fix): added way to choose country

--- components/SelectCounty.js
+++ components/SelectCounty.js
@@ -1,31 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import tw from 'tailwind-styled-components';
+import { Autocomplete, Grid, Skeleton, TextField } from "@mui/material";
+import useAxios from "../hooks/useAxios";
 
-const SelectCounty = () => {
+const SelectCountry = (props) => {
+  const { value, setValue, label } = props;
+  const [data, loaded, error] = useAxios("https://restcountries.com/v3.1/all");
+  console.log(data);
+
+  if (loaded) {
+    return (
+      <Grid item xs={12} md={3}>
+        <Skeleton variant="rounded" height={60} />
+      </Grid>
+    );
+  }
+  if (error) {
+    return "something went wrong!";
+  }
+
+  const dataFilter = data.filter((item) => "currencies" in item);
+  const dataCountries = dataFilter.map((item) => {
+    return `${item.flag} ${Object.keys(item.currencies)[0]} - ${
+      item.name.common
+    }`;
+  });
+
   return (
-        <CountryContainer>
-            <Title>Send send money to</Title>
-            <CountryInput>
-                <Input/>
-            </CountryInput>
-        </CountryContainer>
-  )
-}
+    <Wrapper>
+      <CountryContainer>
+        <Title>Choose Country to send money to </Title>
+          <Autocomplete
+            value={value}
+            disableClearable
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            options={dataCountries}
+            renderInput={(params) => <TextField {...params} label={label} />}
+          />
+      </CountryContainer>
+    </Wrapper>
+  );
+};
 
-export default SelectCounty;
+export default SelectCountry;
+
+const Wrapper = tw.div`
+    flex flex-col h-screen
+`;
 
 const CountryContainer = tw.div`
   flex-1 p-4
 `;
 
 const Title = tw.div`
-  flex-1  text-violet-500 font-bold pl-4
-`;
-
-const CountryInput = tw.div`
-
-`;
-
-const Input = tw.div`
-
+  flex text-violet-400 font-bold pl-4 md:text-4xl self-center
 `;
